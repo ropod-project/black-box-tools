e40e50047ef3dd05819ff60c7f06fcaef04736ba
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-12-11 13:38:48 +0100

    [ros/topic_utils] publish_data can now also take a pymongo Cursor in addition to a list

--- black_box_tools/ros/topic_utils.py
+++ black_box_tools/ros/topic_utils.py
@@ -1,4 +1,5 @@
 from importlib import import_module
+import pymongo as pm
 from rospy_message_converter.message_converter import convert_dictionary_to_ros_message
 import rospy
 
@@ -13,6 +14,8 @@ class TopicUtils(object):
                                  if ROS data are logged) (default True)
     @param queue_size -- queue size for the message publisher (default 1)
 
+    Note: Only a direct mapping between black box data and ROS messages is currently supported.
+
     @author Alex Mitrevski
     @contact aleksandar.mitrevski@h-brs.de
 
@@ -26,47 +29,83 @@ class TopicUtils(object):
         msg_type = self.__get_msg_type(self.msg_type_name)
         self.topic_pub = rospy.Publisher(topic, msg_type, queue_size=queue_size)
 
+    def publish_data(self, dict_msgs, sync_time=True):
+        '''Publishes a list of black box data items to self.topic. If "sync_time"
+        is set to True, synchronises the messages based on the timestamps
+        of the data items.
+
+        @param dict_msgs -- either a list of black box data item dictionaries
+                            or a pymongo.cursor.Cursor instance for retrieving
+                            data item dictionaries
+        @param sync_time -- indicates whether the time between the published
+                            messages should be synchronised based on the
+                            message timestamps (default True)
+
+        '''
+        if isinstance(dict_msgs, list):
+            if dict_msgs:
+                current_msg_time = dict_msgs[0]['timestamp']
+                for i in range(len(dict_msgs)-1):
+                    self.publish_dict(dict_msgs[i])
+                    if sync_time:
+                        # we find the delay between two consecutive messages and sleep
+                        next_msg_time = dict_msgs[i+1]['timestamp']
+                        msg_time_delta = next_msg_time - current_msg_time
+                        print('Sleeping for {0}s'.format(msg_time_delta))
+                        rospy.sleep(msg_time_delta)
+                        current_msg_time = next_msg_time
+                self.publish_dict(dict_msgs[-1])
+            else:
+                print('[publish_data] dict_msgs empty; nothing to publish')
+        elif isinstance(dict_msgs, pm.cursor.Cursor):
+            dict_msg = {}
+            try:
+                dict_msg = next(dict_msgs)
+                current_msg_time = None
+                if dict_msg:
+                    current_msg_time = dict_msg['timestamp']
+
+                while dict_msg:
+                    self.publish_dict(dict_msg)
+                    if sync_time:
+                        # we find the delay between two consecutive messages and sleep
+                        dict_msg = next(dict_msgs)
+                        next_msg_time = dict_msg['timestamp']
+                        msg_time_delta = next_msg_time - current_msg_time
+                        print('Sleeping for {0}s'.format(msg_time_delta))
+                        rospy.sleep(msg_time_delta)
+                        current_msg_time = next_msg_time
+            except StopIteration:
+                if dict_msg:
+                    self.publish_dict(dict_msg)
+        else:
+            print('[publish_data] dict_msgs has to be either a list or a pymongo.cursor.Cursor')
+
     def publish_dict(self, dict_msg):
         '''Publishes the given item to self.topic.
 
         @param dict_msg -- a black box data item dictionary
 
         '''
+        msg = self.__dict_to_msg(dict_msg)
+        if msg:
+            self.topic_pub.publish(msg)
+        else:
+            print('[publish_dict] The input dictionary could not be converted to a ROS message')
+
+    def __dict_to_msg(self, dict_msg):
+        '''Converts the input dictionary to a ROS message of type "self.msg_type_name".
+
+        @param dict_msg -- a black box data item dictionary
+
+        '''
+        msg = None
         if self.direct_msg_mapping:
             msg = convert_dictionary_to_ros_message(self.msg_type_name, dict_msg)
-            self.topic_pub.publish(msg)
         else:
             # TODO: define a procedure to map indirect message correspondences to ROS messages
             pass
-
-    def publish_data(self, dict_msgs, sync_time=True):
-        '''Publishes a list of black box data items to self.topic. If "sync_time"
-        is set to True, synchronises the messages based on the timestamps
-        of the data items.
-
-        @param dict_msgs -- a list of black box data item dictionaries
-        @param sync_time -- indicates whether the time between the published
-                            messages should be synchronised based on the
-                            message timestamps (default True)
-
-        '''
-        if dict_msgs:
-            current_msg_time = dict_msgs[0]['timestamp']
-            for i in range(len(dict_msgs)-1):
-                msg = None
-                if self.direct_msg_mapping:
-                    msg = convert_dictionary_to_ros_message(self.msg_type_name, dict_msgs[i])
-                else:
-                    # TODO: define a procedure to map indirect message correspondences to ROS messages
-                    pass
-                self.topic_pub.publish(msg)
-
-                if sync_time:
-                    # we find the delay between two consecutive messages and sleep
-                    next_msg_time = dict_msgs[i+1]['timestamp']
-                    msg_time_delta = next_msg_time - current_msg_time
-                    rospy.sleep(msg_time_delta)
-                    current_msg_time = next_msg_time
+        return msg
 
     def __get_msg_type(self, msg_type_name):
         '''Returns a "type" corresponding to the type of the input message.
