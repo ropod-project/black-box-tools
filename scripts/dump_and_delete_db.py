1da6ee82273124f5ce25ec1a9a970062493b2570
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2019-06-18 18:47:44 +0200

    [scripts/dump_and_delete_db] Fixed incorrect processing of bool arg

--- scripts/dump_and_delete_db.py
+++ scripts/dump_and_delete_db.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import ast
 import argparse
 
 from black_box_tools.db_utils import DBUtils
@@ -7,12 +8,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Dump database (and optionally delete it afterwards)')
     parser.add_argument('-db', help='name of the mongo db', default='logs')
     parser.add_argument('-path', help='directory of the dump', default='.')
-    parser.add_argument('-delete', help='name of the mongo db', default=True)
+    parser.add_argument('-delete', help='name of the mongo db', default='True')
 
     args = parser.parse_args()
     db_name = args.db
     dump_path = args.path
-    delete_db = args.delete
+    delete_db = ast.literal_eval(args.delete)
 
     print('Dumping database {0} to {1}'.format(db_name, dump_path))
     DBUtils.dump_db(db_name=db_name, data_dir=dump_path, delete_db=delete_db)
